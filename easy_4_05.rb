d1c5a844171447268b02872e7ebe7001820950ed
Jason Nelson <Jasonn318@gmail.com>
2017-07-10 17:31:31 -0500

    add easy_4_05.rb

---
+++ easy_4_05.rb
@@ -0,0 +1,27 @@
+# Write a method that searches for all multiples of 3 or 5 that lie between 1 and some other number, and then computes the sum of those multiples. For instance, if the supplied number is 20, the result should be 98 (3 + 5 + 6 + 9 + 10 + 12 + 15 + 18 + 20).
+
+# You may assume that the number passed in is an integer greater than 1.
+count = 0
+def sum_mult_five_three int
+  sum = 0
+  arr = (1..int).map{|x| x}
+  arr.each do |x| 
+    sum += x if x % 3 == 0 || x % 5 == 0
+  end 
+  sum
+end
+
+# ALTERNATIVE APPROACH
+# def multiple?(number, divisor)
+#   number % divisor == 0
+# end
+
+# def multisum(max_value)
+#   sum = 0
+#   1.upto(max_value) do |number|
+#     if multiple?(number, 3) || multiple?(number, 5)
+#       sum += number
+#     end
+#   end
+#   sum
+# end
