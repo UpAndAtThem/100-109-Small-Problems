317f13ed20072e71e7e9026de86ed854e3b408d2
Jason Nelson <Jasonn318@gmail.com>
2017-07-18 14:52:37 -0500

    add alternative approach as comment

--- easy_4_08.rb
+++ easy_4_08.rb
@@ -23,3 +23,12 @@ def string_to_integer(str)
 end
 
 string_to_integer '+12345' # returns the integer -12345
+
+# ALTERNATIVE APPROACH use method from easy_4_07.rb
+# def string_to_signed_integer(string)
+#   case string[0]
+#   when '-' then -string_to_integer(string[1..-1])
+#   when '+' then string_to_integer(string[1..-1])
+#   else          string_to_integer(string)
+#   end
+# end
