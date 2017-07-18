657113094e629fcebfc4f0313d4f3f920e8e66ba
Jason Nelson <Jasonn318@gmail.com>
2017-07-18 14:44:42 -0500

    refactored and added positive sign prefix check of argument

--- easy_4_08.rb
+++ easy_4_08.rb
@@ -1,3 +1,7 @@
+# Write a method that takes a String of digits, and returns the appropriate number as an integer.
+# The String may have a leading + or - sign; if the first character is a +, your method should return a positive number
+# if it is a -, your method should return a negative number. If no sign is given, you should return a positive number.
+
 NUMBERS = { 1 => '1', 2 => '2', 3 => '3', 4 => '4', 5 => '5',
             6 => '6', 7 => '7', 8 => '8', 9 => '9', 0 => '0' }
 
@@ -5,6 +9,7 @@ def string_to_integer(str)
   neg = true if str[0] == '-'
   num_str_arr = str.split('')
   num_str_arr.delete '-'
+  num_str_arr.delete '+'
   int_arr = []
   num_str_arr.map do |digit|
     int_arr << NUMBERS.key(digit)
@@ -17,4 +22,4 @@ def string_to_integer(str)
   value
 end
 
-string_to_integer '-12345' # returns the integer -12345
+string_to_integer '+12345' # returns the integer -12345
