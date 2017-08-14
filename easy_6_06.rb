1e2ef8a0ad24b5b4f1a103060a0a097af9df84f7
Jason Nelson <Jasonn318@gmail.com>
2017-08-14 14:56:59 -0500

    add easy_6_06.rb

---
+++ easy_6_06.rb
@@ -0,0 +1,15 @@
+# Write a method that takes two Arrays as arguments, and returns an Array that contains all of the values from the argument Arrays. There should be no duplication of values in the returned Array, even if there are duplicates in the original Arrays.
+
+# Example
+
+def merge(array1, array2)
+  (array1 << array2).flatten.uniq
+end
+
+p merge([1, 3, 5], [3, 6, 9]) == [1, 3, 5, 6, 9]  #=> true
+
+# ALTERNATIVE APPROACH
+
+# def merge(array_1, array_2)
+#   array_1 | array_2
+# end
