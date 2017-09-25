0a80fc3d79cb80785029b5e2690483afe718b1b0
Jason Nelson <Jasonn318@gmail.com>
2017-09-25 11:47:58 -0500

    refactored display_diamond method into 2 methods one that handles the logic and one the printing

--- medium_1_05.rb
+++ medium_1_05.rb
@@ -1,19 +1,21 @@
-def display_diamond(size)
-  count = 1
-  rows_from_center = size / 2
-  rows_from_center.times do |_|
+def print_half_diamond(distance, size, is_top)
+  count = is_top ? 1 : (size - 2) 
+  count_change = 2
+  distance.times do |_|
     p ("*" * count).center size
-    count += 2
-  end
-  p ("*" * size).center size
-  count = size - 2
-  rows_from_center.times do |_|
-    p ("*" * count).center size
-    count -= 2
+    count += count_change if is_top
+    count -= count_change if !is_top
   end
 end
 
-display_diamond 23
+def display_diamond(size)
+  rows_from_center = size / 2
+  print_half_diamond rows_from_center, size, true
+  p ("*" * size).center size
+  print_half_diamond rows_from_center, size, false
+end
+
+display_diamond 19
 
 # ALTERNATIVE APPROACH
 # def print_row(grid_size, distance_from_center)
