22124c67bb415f500210e047aea18cabec7866bc
Jason Nelson <Jasonn318@gmail.com>
2018-01-03 15:52:35 -0600

    refactored to use map shorthand split and reduce in the one line solution

--- easy_1_09.rb
+++ easy_1_09.rb
@@ -5,16 +5,9 @@
 # puts sum(496) == 19
 # puts sum(123_456_789) == 45
 
-def sum pos_int
-  sum = 0
-  int_arr = pos_int.to_s.split("")
-  int_arr = int_arr.map { |s| s.to_i } # take each string number and turn to an integer number
-  int_arr.each{ |num| sum += num }
-  return sum
+def sum(int)
+  int.to_s.split("").map(&:to_i).reduce(&:+)
 end
-
-p sum 101_010_101_010
-
 
 # #ALTERNATIVE APPROACH
 # def sum(number)
