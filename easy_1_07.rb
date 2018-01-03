e9840407056580ad432f927b0fa0ce8a701d27e1
Jason Nelson <Jasonn318@gmail.com>
2018-01-03 15:06:07 -0600

    refactored to use the ternary operator and the += addition assignment

--- easy_1_07.rb
+++ easy_1_07.rb
@@ -1,22 +1,10 @@
 #Write a method that takes one argument, a positive integer, and returns a string of alternating 1s and 0s, always starting with 1. The length of the string should match the given integer.
 
-def onezero num
-  #print num times, alternating 1's and 0's starting with 1
-  return_val = ""
-
-  num.times do |x|
-    return_val << '0' if x % 2 != 0
-    return_val << '1' if x % 2 == 0
-  end
-
-  return_val
+def stringy(length)
+  result = ''
+  length.times { |current_num| result += current_num % 2 == 0 ? '1' : '0' }
+  result
 end
-
-p onezero 6
-p onezero 9
-p onezero 4
-p onezero 7
-
 
 # These should evaluate to true
 # puts stringy(6) == '101010'
