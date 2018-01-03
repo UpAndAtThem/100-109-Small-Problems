9f322b8bb1bd523eb709f8eefffa80f9ada7cd73
Jason Nelson <Jasonn318@gmail.com>
2018-01-03 15:12:38 -0600

    refactored using the shorthand implementation of the reduce method and finding the average

--- easy_1_08.rb
+++ easy_1_08.rb
@@ -4,13 +4,9 @@
 # puts average([1, 5, 87, 45, 8, 8]) == 25
 # puts average([9, 47, 23, 95, 16, 52]) == 40
 
-def average arr
-  tot = arr.inject {|sum, i| sum + i} #sum is the accumulator in this method.  Which each i element in the collection will be added to sum.
-  tot.to_f/arr.size 
+def average(int_arr)
+  int_arr.reduce(&:+)/int_arr.length
 end
-p average [1, 5, 87, 45, 8, 8]
-p average([1, 5, 87, 45, 8, 8]) == 25
-p average([9, 47, 23, 95, 16, 52]) == 40
 
 # ALTERNATIVE APPROACH
 
