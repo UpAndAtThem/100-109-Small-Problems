f25b421f294d906e2d9d3ec99a718e044e55731b
Jason Nelson <Jasonn318@gmail.com>
2018-01-29 18:31:29 -0600

    refactored the iterators in rotate method to be more clear in the  .times variable declaration from i and j to length and height.

--- advanced_1_05.rb
+++ advanced_1_05.rb
@@ -1,12 +1,14 @@
 def rotate(matrix, degree)
   return_matrix = []
   matrix_row = []
+  row_length = matrix[0].length
+  matrix_size = matrix.length
 
-  matrix[0].length.times do |i|
-    matrix_row = []
+  row_length.times do |length|
+    matrix_row.clear
 
-    matrix.length.times do |j|
-      matrix_row << matrix[j][i]
+    matrix_size.times do |height|
+      matrix_row << matrix[height][length]
     end
 
     return_matrix << matrix_row.reverse
@@ -28,8 +30,7 @@ matrix2 = [
 ]
 
 p new_matrix1 = rotate(matrix1, 180)
-new_matrix2 = rotate(matrix2, 180)
+p new_matrix2 = rotate(matrix2, 180)
 
-new_matrix1 == [[6, 9, 3], [2, 7, 4], [8, 5, 1]]
-new_matrix2 == [[8, 0, 1, 5], [2, 4, 7, 3]]
-
+p new_matrix1 == [[6, 9, 3], [2, 7, 4], [8, 5, 1]]
+p new_matrix2 == [[8, 0, 1, 5], [2, 4, 7, 3]]
