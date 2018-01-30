2537b2f15fe5651588d53c47a4952a001aa74283
Jason Nelson <Jasonn318@gmail.com>
2018-01-30 12:30:16 -0600

    add advanced_1_01.rb

---
+++ advanced_1_01.rb
@@ -0,0 +1,17 @@
+ADJECTIVES = %w(lazy perky white agressive cantankerous)
+NOUNS = %w(bridge tunnel bird democracy money glass book)
+VERBS = %w(dashed jumped crawled flossed skiped alerted)
+ADVERBS = %w(easily lazily quickly mercilessly combatively)
+
+madlibs_card = File.open("./madlibs_card.txt", "r") do |file|
+                 file.each do |line|
+                   puts format(line, noun: NOUNS.sample,
+                                     adjective: ADJECTIVES.sample,
+                                     adverb: ADVERBS.sample,
+                                     verb: VERBS.sample)
+                 end
+               end
+
+word = "hello"
+
+# p format("%(word) world", word)
