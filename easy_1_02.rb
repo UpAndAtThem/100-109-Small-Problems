a86c24df9158c594dfc3e3e8ef24dfb2acb95463
Jason Nelson <Jasonn318@gmail.com>
2018-05-28 11:48:48 -0500

    refactored out the ternary and added pseudo code

--- easy_1_02.rb
+++ easy_1_02.rb
@@ -1,10 +1,23 @@
-#Write a method that takes one integer argument, which may be positive, negative, or zero. This method return true if the number's absolute value is odd. You may assume that the argument is a valid integer value.
+# Write a method that takes one integer argument, which may be positive, negative, or zero.
+# This method return true if the number's absolute value is odd. You may assume that the argument is a valid integer value.
 
-def is_odd? int
-  int.abs % 2 != 0 ? true : false
+# -----pseudo code------
+
+# given an integer
+
+# check to see if integer modulo two is not equivalent to zero
+# return boolean value from the modulus (remainder) operation
+
+# ------formal pseudo code-------
+
+# START
+
+# given an integer initialized at a method level scope named int
+
+# PRINT int % 2 != 0
+
+# END 
+
+def is_odd?(int)
+  int.odd?
 end
-
-p is_odd? -2
-p is_odd? -123
-
-
