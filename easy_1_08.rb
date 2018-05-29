0c3313f0d2030bc5f7be26136b8cdd606e93ac39
Jason Nelson <Jasonn318@gmail.com>
2018-05-29 17:34:44 -0500

    rubocop refactoring and added pseudo code

--- easy_1_08.rb
+++ easy_1_08.rb
@@ -1,16 +1,31 @@
-#Write a method that takes one argument, an array containing integers, and returns the average of all numbers in the array. The array will never be empty and the numbers will always be positive integers.
+# Write a method that takes one argument, an array containing integers,
+# and returns the average of all numbers in the array. The array
+# will never be empty and the numbers will always be positive integers.
 
-# these should evaluate to true
-# puts average([1, 5, 87, 45, 8, 8]) == 25
-# puts average([9, 47, 23, 95, 16, 52]) == 40
+# ----pseudo code----
+
+# given an array of integers named int_arr
+# initialize variable sum equal to 0
+# iterate over int_arr where each iteration variable is named int
+#   reassign sum as, sum + int
+# return sum divided by the number of elements in int_arr
+
+# ----formal pseudo code----
+
+# START
+
+# given an array of integers
+
+# SET sum = 0, counter = 1
+
+# WHILE counter <= num of elements in int_arr
+#   sum += element at position 'counter' of int_arr
+#   counter += 1
+#
+# PRINT sum / number of elements in int_arr
+
+# END
 
 def average(int_arr)
-  int_arr.reduce(&:+)/int_arr.length
+  int_arr.reduce(&:+) / int_arr.size
 end
-
-# ALTERNATIVE APPROACH
-
-# def average(numbers)
-#   sum = numbers.reduce { |sum, number| sum + number }
-#   sum / numbers.count
-# end
