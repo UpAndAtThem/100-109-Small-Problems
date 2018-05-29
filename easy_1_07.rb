68912e3fdf3a3347741e42acc84458d9771e55ac
Jason Nelson <Jasonn318@gmail.com>
2018-05-29 17:00:46 -0500

    refactored stringy to use up_to instead of times. with_object instead of initializing in prior line

--- easy_1_07.rb
+++ easy_1_07.rb
@@ -1,13 +1,40 @@
-#Write a method that takes one argument, a positive integer, and returns a string of alternating 1s and 0s, always starting with 1. The length of the string should match the given integer.
+# Write a method that takes one argument, a positive integer,
+# and returns a string of alternating 1s and 0s, always starting with 1.
+# The length of the string should match the given integer.
 
-def stringy(length)
-  result = ''
-  length.times { |current_num| result += current_num % 2 == 0 ? '1' : '0' }
-  result
+# ----pseudo code----
+# given a positive integer named length
+# initialize counter set to 0
+# initialize result_string to ''
+# loop length number of times
+#   -if counter is even
+#        add "1" to result_string
+#     -otherwise
+#        add '0' to result_string
+# return result_string
+
+# ----formal pseudo code----
+
+# START
+
+# given a positive integer named length
+
+# counter = 1, result_string = ''
+
+# While counter <= length
+#   IF counter is odd
+#     result_string += "1"
+#   ELSE
+#     result_string += "0"
+
+#   counter += 1
+
+# PRINT result_string
+
+# END
+
+def stringy(int)
+  1.upto(int).with_object('') do |num, result_string|
+    result_string << (num.odd? ? '1' : '0')
+  end
 end
-
-# These should evaluate to true
-# puts stringy(6) == '101010'
-# puts stringy(9) == '101010101'
-# puts stringy(4) == '1010'
-# puts stringy(7) == '1010101'
