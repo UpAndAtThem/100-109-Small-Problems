f754f8833f8df5e2b02ef5f245fee56b9634d2cb
Jason Nelson <Jasonn318@gmail.com>
2018-05-29 18:03:23 -0500

    refactored code to use ternary as implicit return

--- easy_1_10.rb
+++ easy_1_10.rb
@@ -1,21 +1,27 @@
-#Write a method that takes two arguments, a positive integer and a boolean, and calculates the bonus for a given salary. If the boolean is true, the bonus should be half of the salary. If the boolean is false, the bonus should be 0.
+# Write a method that takes two arguments, a positive integer and a boolean,
+# and calculates the bonus for a given salary. If the boolean is true,
+# the bonus should be half of the salary.
+# If the boolean is false, the bonus should be 0.
 
-# These expressions should evaluate to true
-# puts calculate_bonus(2800, true) == 1400
-# puts calculate_bonus(1000, false) == 0
-# puts calculate_bonus(50000, true) == 25000
+# ----pseudo code----
+# given an int named salary, and a bool named gets_bonus
 
-def calculate_bonus pos_int, bool
-  return bonus = pos_int / 2 if bool == true
-  return 0 if bool == false
+# -if gets_bonus is true return salary / 2
+# -otherwise return 0
+
+# ----formal pseudo code----
+
+# START
+
+# given an int named salary, and a bool named gets_bonus
+# SET bonus_amount = 0
+# IF gets_bonus is true
+#   - bonus_amount = salary / 2
+
+# PRINT bonus_amount
+
+# END
+
+def calculate_bonus(salary, gets_bonus)
+  gets_bonus ? salary / 2 : 0
 end
-
-puts calculate_bonus(2800, true) == 1400
-puts calculate_bonus(1000, false) == 0
-puts calculate_bonus(50000, true) == 25000
-
-# ALTERNATIVE METHOD
-
-# def calculate_bonus(salary, bonus)
-#   bonus ? (salary / 2) : 0
-# end
